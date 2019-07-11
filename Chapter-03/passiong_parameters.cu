aa23eea72ad775e73d27ef82f4b9b563dadcf7c1
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-07-11 11:23:31 +0800

    add source code for the book's samples & a new source code for chapter 3.2

---
+++ Chapter-03/passiong_parameters.cu
@@ -0,0 +1,15 @@
+#include <iostream>
+
+__global__ void add(int a, int b, int *c);
+
+int main(void) {
+  int c;
+  int *dev_c;
+
+  HANDLE_ERROR(cudaMalloc((void **)&dev_c, sizeof(int)));
+  add<<<1, 1>>>(2, 7, dev_c);
+  HANDLE_ERROR(cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost));
+  printf("2 + 7 = %d \n", c);
+  cudaFree(dev_c);
+  return 0;
+}
